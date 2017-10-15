a866793d0ef54365f5b86d4b4beb6bee1ebb7129
BENMALEKarim <32803877+BENMALEKarim@users.noreply.github.com>
2017-10-15 15:14:11 +0200

    Update VectorHelper.java

--- TP_IGL/src/com/company/VectorHelper.java
+++ TP_IGL/src/com/company/VectorHelper.java
@@ -2,28 +2,7 @@ package com.company;
 
 public class VectorHelper {
 
-    public int [] sommeVec(int [] v1, int [] v2) throws TailleException
-    {
 
-            if(v1.length != v2.length) throw new TailleException() ;
-            else {
-                int [] v3 = new int[v1.length];
-                for ( int i = 0 ; i < v1.length ; i++) {
-                    v3[i] = v2[i] + v1[i] ;
-                }
-                return v3 ;
-            }
-    }
-
-    public int [] formule(int [] v){
-        int [] w = new int[v.length] ;
-        for (int i=0 ; i<v.length; i ++){
-            w[i] = v[i] * 2;
-        }
-        return w;
-
-
-    }
     public void tri(int[] v) {
         int m ;
         for ( int i = 0 ; i < v.length ; i++){
